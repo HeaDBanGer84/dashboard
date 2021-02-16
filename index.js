4bcdb7c9d2ff9bf80c5473e707d7f8a3f6914b43
Harald Bongen <bongen@fh-aachen.de>
2021-02-16 20:18:53 +0100

    adding an frontend

--- index.js
+++ index.js
@@ -7,29 +7,40 @@ let docker = new Docker({ socketPath: '/var/run/docker.sock' });
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.use('/', express.static('public'))
+
+app.get('/config', (req, res) => {
     console.log(req.url)
     docker.listContainers(function (err, containers) {
         let resp = []
         if (containers) {
             containers.forEach(function (containerInfo) {
-                console.log(containerInfo.Id)
+                //console.log(containerInfo.Id)
                 let composeService = containerInfo.Labels['com.docker.compose.service'];
                 let extractedURL = containerInfo.Labels['traefik.http.routers.' + composeService + '.rule'];
                 const regex = /^Host\(\`(.*)\`\)/gm;
 
                 let match = regex.exec(extractedURL)
-                if(match){
-                    extractedURL=match[1]
+                if (match) {
+                    extractedURL = match[1]
+                    let icon=containerInfo.Labels['dash.icon.fa'] ?? "fas fa-globe";
+                    let color=containerInfo.Labels['dash.icon.color'] ?? "#111111";
+
+                    resp.push({
+                        "alt": composeService,
+                        "icon": icon,
+                        "color": color,
+                        "link": "http://" + extractedURL
+                        //"containerInfo": containerInfo,
+                    })
                 }
-                resp.push({
-                    //"containerInfo": containerInfo,
-                    "name": composeService,
-                    "url": "http://"+extractedURL
-                })
+                
             });
         }
-        res.json(resp);
+        res.json({
+            title: "dashboard",
+            items: resp
+        });
     });
 })
 
---
+++ public/common/js/main.js
@@ -0,0 +1,35 @@
+const req = new Request('/config');
+fetch(req)
+    .then(response => response.json())
+    .then(config => {
+        document.title = config.title;
+        var itemlistHTML = '';
+        config.items.forEach(item => {
+            itemlistHTML += '<a href="' + item.link + '" title="' + item.alt + '" ><i class="' + item.icon + ' fa-fw" style="color:' + item.color + ';"></i><strong>' + item.alt + '</strong></a>';
+        });
+        document.getElementById("itemlist").innerHTML = itemlistHTML;
+    })
+
+function addTriangleTo(target) {
+    var dimensions = target.getClientRects()[0];
+    var pattern = Trianglify({
+        width: dimensions.width,
+        height: dimensions.height
+    });
+
+    target.style['background-image'] = 'url(' + pattern.png() + ')';
+    target.style['background-size'] = 'cover';
+    target.style['-webkit-background-size'] = 'cover';
+    target.style['-moz-background-size'] = 'cover';
+    target.style['-o-background-size'] = 'cover';
+}
+
+var resizeTimer;
+window.addEventListener("resize", function () {
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(function () {
+        addTriangleTo(homepage);
+    }, 400);
+})
+
+addTriangleTo(homepage);
